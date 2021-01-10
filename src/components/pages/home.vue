97152a3d45b47007e913454f8485199c565ee8fa
daviddarx <daviddarx@gmail.com>
2021-01-10 12:34:37 +0100

    feat(home) links content from admin

--- src/components/pages/home.vue
+++ src/components/pages/home.vue
@@ -7,30 +7,28 @@
     <div class="content__container">
 
       <h2 class="visually-hidden">
-        Home
+        {{home.title}}
       </h2>
 
-      <p>
-        Unser Lebensweg wird begleitet von einem roten Faden, der sich als fortlaufender Prozess durch unser Leben schlängelt.
-      </p>
-
-      <p>
-        Auf diesem individuellen Lebensweg sind wir ständig in Beziehung und Bewegung, halten dazwischen inne, begegnen Anderen, sind manchmal im Zwiespalt, machen Seitensprünge, überkreuzen Wege von Neuen und Neuem, kommen manchmal vom Wege ab, sind unentschlossen, verlassen und wissen auch nicht immer wie weiter.
-      </p>
-
-      <p>
-        Oft müssen wir Entscheidungen fällen auf diesem Lebensweg. In meiner Beratung möchte ich mit Ihnen einen Raum ausgestalten, indem weniger „entweder oder“, aber mehr „sowohl als auch“ entstehen und möglich sein kann.
-      </p>
+      <div
+        class="content__md"
+        v-html="getHTMLfromMD(home.content)"
+      >
+      </div>
     </div>
   </div>
 </template>
 
 <script>
   import Vue from "vue";
+  import * as home from '../../../content/home/home.json';
+  import getHTMLfromMDMixin from '../../mixins/getHTMLFromMD';
 
   export default Vue.extend({
+    mixins: [getHTMLfromMDMixin],
     data() {
       return {
+        home: home,
         isDisplayed : false,
       }
     },
