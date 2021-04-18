02f89e35e91deaaf80531925e23468ab63a782dc
daviddarx <daviddarx@gmail.com>
2021-04-18 21:51:07 +0200

    feat(contact) adds google link

--- src/components/contact.vue
+++ src/components/contact.vue
@@ -3,7 +3,7 @@
   <div class="contact">
     <span class="contact__address">
       Praxis Lutherhof<br>
-      Lutherstrasse 6, 8004 Zürich<br><br>
+      <a href="https://www.google.com/maps/place/Lutherstrasse+6,+8004+Z%C3%BCrich/@47.3739382,8.5274439,17z/data=!3m1!4b1!4m5!3m4!1s0x47900a1b36fadf71:0x48a0386d9f795e62!8m2!3d47.3739382!4d8.5296379" target="blank"> Lutherstrasse 6, 8004 Zürich</a><br><br>
     </span>
     +41 79 614 26 18<br>
     <a href="mailto:kontakt@tinabeerli.ch" target="_blank">kontakt@tinabeerli.ch</a>
