5d420450057592ee382253f582d3e6502a98e5aa
daviddarx <daviddarx@gmail.com>
2021-06-15 20:13:58 +0200

    fix(navigation) sets correct email address

--- src/components/navigation.vue
+++ src/components/navigation.vue
@@ -43,7 +43,7 @@
           <a href="https://www.google.com/maps/place/Lutherstrasse+6,+8004+Z%C3%BCrich/@47.3739382,8.5274439,17z/data=!3m1!4b1!4m5!3m4!1s0x47900a1b36fadf71:0x48a0386d9f795e62!8m2!3d47.3739382!4d8.5296379" target="blank"> Lutherstrasse 6, 8004 Zürich</a><br><br>
         </span>
         +41 79 614 26 18<br>
-        <a href="mailto:hallo@tinabeerli.ch" target="_blank">hallo@tinabeerli.ch</a>
+        <a href="mailto:kontakt@tinabeerli.ch" target="_blank">kontakt@tinabeerli.ch</a>
       </div>
     </div>
 
