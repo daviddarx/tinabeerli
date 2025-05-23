a153f8c5ae2a473c4b057b960807a214111c7bfc
David Darx <daviddarx@gmail.com>
2025-05-23 14:52:09 +0200

    feat(setup): update logo subline

--- src/app.vue
+++ src/app.vue
@@ -1,4 +1,3 @@
-
 <template>
   <div
     class="website-container"
@@ -8,7 +7,9 @@
       'is-nav-absolute': this.isNavAbsolute
     }"
   >
-    <h1 class="visually-hidden">Tina Beerli – Psychologische Beratung &amp; Coaching</h1>
+    <h1 class="visually-hidden">
+      Tina Beerli – Psychologische Einzel- &amp; Beziehungsberatung
+    </h1>
 
     <logo></logo>
 
@@ -19,11 +20,7 @@
     <portrait></portrait>
 
     <div class="content">
-      <router-view
-        :key="$route.fullPath"
-        ref="view"
-      >
-      </router-view>
+      <router-view :key="$route.fullPath" ref="view"> </router-view>
 
       <custom-contact class="contact--mobile"></custom-contact>
 
@@ -33,81 +30,84 @@
     <custom-contact></custom-contact>
 
     <custom-transition></custom-transition>
-
   </div>
 </template>
 
 <script>
-  import Vue from "vue";
-  import Logo from './components/logo.vue';
-  import CustomNavigation from './components/navigation.vue';
-  import CustomFooter from './components/footer.vue';
-  import CustomContact from './components/contact.vue';
-  import CustomTransition from './components/transition.vue';
-  import Portrait from './components/portrait.vue';
-  import browserDetect from './browser-detect';
-  import BrowserWarning from './components/browser-warning.vue';
+import Vue from "vue";
+import Logo from "./components/logo.vue";
+import CustomNavigation from "./components/navigation.vue";
+import CustomFooter from "./components/footer.vue";
+import CustomContact from "./components/contact.vue";
+import CustomTransition from "./components/transition.vue";
+import Portrait from "./components/portrait.vue";
+import browserDetect from "./browser-detect";
+import BrowserWarning from "./components/browser-warning.vue";
 
+export default Vue.extend({
+  components: {
+    logo: Logo,
+    "custom-navigation": CustomNavigation,
+    "custom-footer": CustomFooter,
+    "custom-contact": CustomContact,
+    "custom-transition": CustomTransition,
+    portrait: Portrait,
+    "browser-warning": BrowserWarning
+  },
+  data() {
+    return {
+      isiOS: false,
+      isAndroid: false,
+      splashScreen: undefined,
+      isLoaded: false,
+      isNavAbsolute: false,
+      displayAfterLoadedDelay: 500
+    };
+  },
+  computed: {},
+  mounted() {
+    this.splashScreen = document.querySelector(".splash");
 
-  export default Vue.extend({
-    components: {
-      'logo': Logo,
-      'custom-navigation': CustomNavigation,
-      'custom-footer': CustomFooter,
-      'custom-contact': CustomContact,
-      'custom-transition': CustomTransition,
-      'portrait': Portrait,
-      'browser-warning': BrowserWarning
+    document.fonts.ready.then(() => {
+      this.isLoaded = true;
+
+      this.splashScreen.classList.add("hidden");
+    });
+
+    this.isiOS = browserDetect.isiOS;
+    this.isAndroid = browserDetect.isAndroid;
+
+    this.checkIfUserTouch();
+
+    window.addEventListener("resize", this.resizeListener);
+    this.resizeListener();
+  },
+  methods: {
+    checkIfUserTouch: function() {
+      window.isTouch = false;
+      window.addEventListener("touchstart", this.touchListener);
     },
-    data() {
-      return {
-        isiOS: false,
-        isAndroid: false,
-        splashScreen: undefined,
-        isLoaded: false,
-        isNavAbsolute: false,
-        displayAfterLoadedDelay: 500,
-      }
+    touchListener: function(e) {
+      window.isTouch = true;
+      document.body.classList.add("is-touch");
+      window.removeEventListener("touchstart", this.touchListener);
     },
-    computed: {
-    },
-    mounted () {
-      this.splashScreen = document.querySelector('.splash');
-
-      document.fonts.ready.then(() => {
-        this.isLoaded = true;
-
-        this.splashScreen.classList.add('hidden');
-      });
-
-      this.isiOS = browserDetect.isiOS;
-      this.isAndroid = browserDetect.isAndroid;
-
-      this.checkIfUserTouch();
-
-      window.addEventListener('resize', this.resizeListener);
-      this.resizeListener();
-    },
-    methods: {
-      checkIfUserTouch: function () {
-        window.isTouch = false;
-        window.addEventListener('touchstart', this.touchListener);
-      },
-      touchListener: function (e) {
-        window.isTouch = true;
-        document.body.classList.add('is-touch');
-        window.removeEventListener('touchstart', this.touchListener);
-      },
-      resizeListener: function () {
-        const width  = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-        const height = window.innerHeight|| document.documentElement.clientHeight|| document.body.clientHeight;
-        const screenRatio = width/height;
-        if (screenRatio > 2.6 && width > this.$refs.nav.isMobileStep) {
-          this.isNavAbsolute = true;
-        } else {
-          this.isNavAbsolute = false;
-        }
+    resizeListener: function() {
+      const width =
+        window.innerWidth ||
+        document.documentElement.clientWidth ||
+        document.body.clientWidth;
+      const height =
+        window.innerHeight ||
+        document.documentElement.clientHeight ||
+        document.body.clientHeight;
+      const screenRatio = width / height;
+      if (screenRatio > 2.6 && width > this.$refs.nav.isMobileStep) {
+        this.isNavAbsolute = true;
+      } else {
+        this.isNavAbsolute = false;
       }
     }
-  });
+  }
+});
 </script>
--- src/components/pages/ueber-mich.vue
+++ src/components/pages/ueber-mich.vue
@@ -1,26 +1,18 @@
-
 <template>
-  <div
-    class="about"
-    v-bind:class="{ 'is-displayed': this.isDisplayed }"
-  >
+  <div class="about" v-bind:class="{ 'is-displayed': this.isDisplayed }">
     <div class="content__container">
       <h2 class="visually-hidden">
-        {{aboutme.title}}
+        {{ aboutme.title }}
       </h2>
 
       <img
         src="../../../static/images/tina_beerli.jpg"
-        alt="Tina Beerli – Tina Beerli – Psychologische Beratung und Coaching"
+        alt="Tina Beerli – Psychologische Einzel- & Beziehungsberatung"
         class="content__portrait"
         ref="image"
       />
 
-      <div
-        class="content__md"
-        v-html="getHTMLfromMD(aboutme.content)"
-      >
-      </div>
+      <div class="content__md" v-html="getHTMLfromMD(aboutme.content)"></div>
     </div>
     <background-decoration
       ref="bgDeco"
@@ -32,37 +24,36 @@
 </template>
 
 <script>
-  import Vue from "vue";
-  import * as aboutmeDE from '../../../content/uebermich_de/uebermich_de.json';
-  import * as aboutmeFR from '../../../content/uebermich_fr/uebermich_fr.json';
-  import getHTMLfromMDMixin from '../../mixins/getHTMLFromMD';
-  import BackgroundDecoration from '../background-decoration.vue';
-  import { i18n } from "../../index.js";
+import Vue from "vue";
+import * as aboutmeDE from "../../../content/uebermich_de/uebermich_de.json";
+import * as aboutmeFR from "../../../content/uebermich_fr/uebermich_fr.json";
+import getHTMLfromMDMixin from "../../mixins/getHTMLFromMD";
+import BackgroundDecoration from "../background-decoration.vue";
+import { i18n } from "../../index.js";
 
-  export default Vue.extend({
-    components: {
-      'background-decoration': BackgroundDecoration
-    },
-    mixins: [getHTMLfromMDMixin],
-    data() {
-      return {
-        aboutme: (i18n.locale =='de') ? aboutmeDE : aboutmeFR,
-        isDisplayed : false,
-      }
-    },
-    mounted () {
-      setTimeout(this.display, 100);
-    },
-    beforeDestroy () {
-      this.$refs.bgDeco.destroy();
-    },
-    methods: {
-      display: function() {
-        if (this.$parent.$parent.isLoaded == true) {
-          this.isDisplayed = true;
-        }
+export default Vue.extend({
+  components: {
+    "background-decoration": BackgroundDecoration
+  },
+  mixins: [getHTMLfromMDMixin],
+  data() {
+    return {
+      aboutme: i18n.locale == "de" ? aboutmeDE : aboutmeFR,
+      isDisplayed: false
+    };
+  },
+  mounted() {
+    setTimeout(this.display, 100);
+  },
+  beforeDestroy() {
+    this.$refs.bgDeco.destroy();
+  },
+  methods: {
+    display: function() {
+      if (this.$parent.$parent.isLoaded == true) {
+        this.isDisplayed = true;
       }
     }
-  });
+  }
+});
 </script>
-
